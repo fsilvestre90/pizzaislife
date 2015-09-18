10c7def2750ca0a495fbfafac17dc9f4bf699a34
Filipe Silvestre <fssilvestre5@gmail.com>
2015-09-18 15:15:13 -0700

    finish project

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,12 @@ function Topping(toppingType) {
 }
 
 // Pizza
-function Pizza(pizzaSize, pizzaType, toppings, pizzaCost) {
+function Pizza(pizzaSize, pizzaType, toppings, quantity, pizzaCost) {
     "use strict";
     this.pizzaSize = pizzaSize;
     this.pizzaType = pizzaType;
     this.toppings = toppings;
+    this.quantity = quantity;
     this.pizzaCost = pizzaCost;
 }
 
@@ -16,17 +17,26 @@ Pizza.prototype.calculatePizzaCost = function() {
     var cost = 0;
 
     //add base pizza price based on size (could make this into array/for loop, but I'm lazy)
-    if (this.pizzaSize === "SM") {
+    switch (this.pizzaSize) {
+        case "small":
         cost += 2;
-    } else if (this.pizzaSize === "MD") {
+        break;
+        case "medium":
         cost += 3;
-    } else if (this.pizzaSize === "LG") {
+        break;
+        case "large":
         cost += 4;
+        break;
     }
     //calclulate cost for each topping (once again this could be dynamic, but I'm lazy)
     for (var topping in this.toppings) {
         cost += 1; //yes, these toppings are hella expensive. non-GMO, organic, and spoken to daily
     }
+    //if there are more than 1 being ordered, multiply the cost with quantity
+    if(this.quantity > 1) {
+        cost = cost * this.quantity;
+    }
+
     //this pizza shop is adventurous and wants to make profit with toppings
     this.pizzaCost = cost;
 };
@@ -45,3 +55,39 @@ Order.prototype.calculateTotal = function() {
     }
     this.orderTotal = pizzaTotal;
 };
+
+//jQuery
+$(document).ready(function() {
+    $('.pizzaSize').on('change', function() {
+        $('input.pizzaSize').not(this).prop('checked', false);
+    });
+
+    $('.pizzaType').on('change', function() {
+        $('input.pizzaType').not(this).prop('checked', false);
+    });
+
+    $("form#new-order").submit(function(event) {
+        event.preventDefault();
+        //get all inputs
+        var toppings = [];
+        $(".toppings:checked").each(function() {
+            toppings.push(new Topping(this.value));
+        });
+        var pizzaSize = $(".pizzaSize:checked").val();
+        var pizzaType = $(".pizzaType:checked").val();
+        var quantity = document.getElementById("quantity").value;
+
+        //create new pizza and calculate the totalcost for it
+        var newPizza = new Pizza(pizzaSize, pizzaType, toppings, quantity);
+        newPizza.calculatePizzaCost();
+        //now make the order and calculate total cost
+        var newOrder = new Order([newPizza]);
+        newOrder.calculateTotal();
+
+        $("#show-order").show();
+        $(".size").text(newPizza.pizzaSize);
+        $(".style").text(newPizza.pizzaType);
+        $(".quantity").text(newPizza.quantity);
+        $(".totalCost").text(newOrder.orderTotal);
+    });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -7,53 +7,53 @@ describe('Topping', function() {
 
 describe('Pizza', function() {
     it("creates a pizza object with no toppings", function() {
-        var testPizza = new Pizza("LG", "BBQ Chicken",[], 17.50);
+        var testPizza = new Pizza("large", "BBQ Chicken",[], 1, 17.50);
         expect(testPizza.toppings).to.eql([]);
         expect(testPizza.pizzaCost).to.equal(17.50);
-        expect(testPizza.pizzaSize).to.equal("LG");
+        expect(testPizza.pizzaSize).to.equal("large");
         expect(testPizza.pizzaType).to.equal("BBQ Chicken");
     });
 
     it("creates a pizza object with 1 topping", function() {
         var pepperoni = new Topping("Pepperoni");
-        var testPizza = new Pizza("LG", "BBQ Chicken",[pepperoni], 17.50);
+        var testPizza = new Pizza("large", "BBQ Chicken",[pepperoni], 1, 17.50);
         expect(testPizza.toppings).to.eql([pepperoni]);
         expect(testPizza.pizzaCost).to.equal(17.50);
-        expect(testPizza.pizzaSize).to.equal("LG");
+        expect(testPizza.pizzaSize).to.equal("large");
         expect(testPizza.pizzaType).to.equal("BBQ Chicken");
     });
     it("creates a pizza object with N toppings", function() {
         var pepperoni = new Topping("Pepperoni");
         var poo = new Topping("Poo");
         var artichoke = new Topping("art");
-        var testPizza = new Pizza("LG", "BBQ Chicken",[pepperoni, poo, artichoke], 17.50);
+        var testPizza = new Pizza("large", "BBQ Chicken",[pepperoni, poo, artichoke], 1, 17.50);
         expect(testPizza.toppings).to.eql([pepperoni, poo, artichoke]);
         expect(testPizza.pizzaCost).to.equal(17.50);
-        expect(testPizza.pizzaSize).to.equal("LG");
+        expect(testPizza.pizzaSize).to.equal("large");
         expect(testPizza.pizzaType).to.equal("BBQ Chicken");
     });
     it("calculates pizza total with 3 toppings", function() {
         var pepperoni = new Topping("Pepperoni");
         var poo = new Topping("Poo");
         var artichoke = new Topping("art");
-        var testPizza = new Pizza("LG", "BBQ Chicken",[pepperoni, poo, artichoke]);
+        var testPizza = new Pizza("large", "BBQ Chicken",[pepperoni, poo, artichoke], 3);
         testPizza.calculatePizzaCost();
-        expect(testPizza.pizzaCost).to.equal(7);
+        expect(testPizza.pizzaCost).to.equal(21);
     });
 });
 
 
 describe('Order', function() {
     it("creates an order", function() {
-        var testPizza = new Pizza("LG", "BBQ Chicken", [], 17.50);
+        var testPizza = new Pizza("large", "BBQ Chicken", [], 1, 17.50);
         var testOrder = new Order([testPizza],17.50);
         expect(testOrder.cartItems).to.eql([testPizza]);
         expect(testOrder.orderTotal).to.equal(17.50);
     });
 
     it("tests total price of pizzas in cart", function() {
-        var bbqChicken = new Pizza("LG", "BBQ Chicken", [], 17.50);
-        var plain = new Pizza("MD", "Plain", [], 11.50);
+        var bbqChicken = new Pizza("large", "BBQ Chicken", [], 1, 17.50);
+        var plain = new Pizza("medium", "Plain", [], 1, 11.50);
         var testOrder = new Order([bbqChicken, plain]);
         testOrder.calculateTotal();
         expect(testOrder.orderTotal).to.equal(29);
@@ -62,8 +62,8 @@ describe('Order', function() {
     it("tests total cart price with total pizza price functions", function() {
         var top1 = new Topping("test");
         var top2 = new Topping("test");
-        var bbqChicken = new Pizza("LG", "BBQ Chicken", [top1, top2]); // should be 6 dollars
-        var plain = new Pizza("MD", "Plain", [top1]); //should be 4
+        var bbqChicken = new Pizza("large", "BBQ Chicken", [top1, top2], 1); // should be 6 dollars
+        var plain = new Pizza("medium", "Plain", [top1], 1); //should be 4
         var testOrder = new Order([bbqChicken, plain]);
         bbqChicken.calculatePizzaCost();
         plain.calculatePizzaCost();
