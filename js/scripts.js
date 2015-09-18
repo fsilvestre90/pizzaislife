28a349d4eec491f60abfa852a0c0a74deb84c605
Filipe Silvestre <fssilvestre5@gmail.com>
2015-09-18 12:10:21 -0700

    finish html page

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,15 @@ Pizza.prototype.calculatePizzaCost = function() {
     var cost = 0;
 
     //add base pizza price based on size (could make this into array/for loop, but I'm lazy)
-    if(this.pizzaSize === "SM") {
+    if (this.pizzaSize === "SM") {
         cost += 2;
-    }
-    else if(this.pizzaSize === "MD") {
+    } else if (this.pizzaSize === "MD") {
         cost += 3;
-    }
-    else if(this.pizzaSize === "LG") {
+    } else if (this.pizzaSize === "LG") {
         cost += 4;
     }
     //calclulate cost for each topping (once again this could be dynamic, but I'm lazy)
-    for(var topping in this.toppings) {
+    for (var topping in this.toppings) {
         cost += 1; //yes, these toppings are hella expensive. non-GMO, organic, and spoken to daily
     }
     //this pizza shop is adventurous and wants to make profit with toppings
@@ -42,7 +40,7 @@ function Order(cartItems, orderTotal) {
 
 Order.prototype.calculateTotal = function() {
     var pizzaTotal = 0;
-    for(var pizzaIndex in this.cartItems) {
+    for (var pizzaIndex in this.cartItems) {
         pizzaTotal += this.cartItems[pizzaIndex].pizzaCost;
     }
     this.orderTotal = pizzaTotal;
