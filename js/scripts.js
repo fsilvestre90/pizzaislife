fc62de254689c39392466206d524b851b3517275
Filipe Silvestre <fssilvestre5@gmail.com>
2015-09-18 11:19:27 -0700

    add some tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,50 @@
-// PLAYER STUFF
-
-function Pizza() {
-    "use strict";
-
+// Topping
+function Topping(toppingType, toppingCost) {
+    this.toppingType = toppingType;
+    this.toppingCost = toppingCost;
 }
 
-// SPACE STUFF
-function Order() {
+// Pizza
+function Pizza(pizzaSize, pizzaType, toppings, pizzaCost) {
     "use strict";
+    this.pizzaSize = pizzaSize;
+    this.pizzaType = pizzaType;
+    this.toppings = toppings;
+    this.pizzaCost = pizzaCost;
+}
+
+// Pizza.prototype.calculatePizzaCost = {
+//     var cost = 0;
+//
+//     //add base pizza price based on size (could make this into array/for loop, but I'm lazy)
+//     if(this.pizzaSize === "SM") {
+//         cost += 2;
+//     }
+//     if(this.pizzaSize === "MD") {
+//         cost += 3;
+//     }
+//     if(this.pizzaSize === "LG") {
+//         cost += 4;
+//     }
+//     //calclulate cost for each topping (once again this could be dynamic, but I'm lazy)
+//     for(var topping in toppings) {
+//         cost += 1; //yes, this pizza will be hella expensive
+//     }
+//     //this pizza shop is adventurous and wants to make profit with toppings
+//     this.pizzaCost = cost;
+// }
+
+// Order
+function Order(cartItems, orderTotal) {
+    "use strict";
+    this.cartItems = cartItems;
+    this.orderTotal = orderTotal;
+}
+
+Order.prototype.calculateTotal = function() {
+    var pizzaTotal = 0;
+    for(var pizzaIndex in this.cartItems) {
+        pizzaTotal += this.cartItems[pizzaIndex].pizzaCost;
+    }
+    this.orderTotal = pizzaTotal;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,39 @@
 describe('Pizza', function() {
-    it("returns the boards", function() {
-        var testBoard = new Board();
-        testBoard.createBoard();
-        expect(testBoard.spaces).to.be.an('array');
+    it("creates a pizza object with no toppings", function() {
+        var testPizza = new Pizza("LG", "BBQ Chicken",[], 17.50);
+        expect(testPizza).to.deep.equal({
+            pizzaSize: "LG",
+            pizzaType: "BBQ Chicken",
+            toppings: [],
+            pizzaCost: 17.50,
+        });
+    });
+    it("creates a pizza object with 1 topping", function() {
+        var pepperoni = new Topping("Pepperoni", .50);
+        var testPizza = new Pizza("LG", "BBQ Chicken",[pepperoni], 17.50);
+        expect(testPizza).to.deep.equal({
+            pizzaSize: "LG",
+            pizzaType: "BBQ Chicken",
+            toppings: [pepperoni],
+            pizzaCost: 17.50,
+        });
     });
 });
 
+
 describe('Order', function() {
-    it("creates two players", function() {
-        var testGame = new Game();
-        testGame.createPlayers();
-        expect(testGame.players).to.eql([testGame.players[0], testGame.players[1]]);
+    it("creates an order", function() {
+        var testPizza = new Pizza("LG", "BBQ Chicken", [], 17.50);
+        var testOrder = new Order([testPizza],17.50);
+        expect(testOrder.cartItems).to.eql([testPizza]);
+        expect(testOrder.orderTotal).to.equal(17.50);
+    });
+
+    it("tests total price of pizzas in cart", function() {
+        var bbqChicken = new Pizza("LG", "BBQ Chicken", [], 17.50);
+        var plain = new Pizza("MD", "Plain", [], 11.50);
+        var testOrder = new Order([bbqChicken, plain]);
+        testOrder.calculateTotal();
+        expect(testOrder.orderTotal).to.equal(29);
     });
 });
